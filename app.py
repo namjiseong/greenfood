12c46dbcfba5b8e8bded4760f806a66074548121
namjiseong <skawltjd2000@naver.com>
2022-03-08 18:43:55 +0900

    index, result 임시 페이지 구성 완료

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 @app.route('/') # 접속하는 url
 def index():
   return render_template('index.html')
-
+@app.route('/result') # 접속하는 url
+def result():
+  return render_template('result.html')
 if __name__ == '__main__':
     app.run()
